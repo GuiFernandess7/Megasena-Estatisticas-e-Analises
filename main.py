8bdf3c762dcde8b2c8c01db692f9647b6dec3947
Guilherme Fernandes <63022500+GuiFernandess7@users.noreply.github.com>
2023-01-08 20:16:39 +0000

    First Commit

---
+++ main.py
@@ -0,0 +1,95 @@
+def app():
+    
+    def load_data(filename):
+        data = pd.read_excel(filename)
+        matrix = data.to_numpy(copy=True)
+        return matrix
+
+    def get_repetition_by_column(matriz, column_index: int, number: int):
+        """Retorna o número de repetições de determinado número em determinada coluna
+        Parâmetros: Os dados, número da bola (```column_index```) e o valor da bola (```number```)"""
+
+        repetitions = np.count_nonzero(matriz[:, column_index] == number)
+        return repetitions
+
+    def get_repetitions_by_number(matriz, number):
+        """Retorna um ```array``` em que cada coluna corresponde ao número de repetições de determinado
+        número (```number```)"""
+
+        repetitions = np.sum(np.equal(matriz, number), axis=0)
+        return repetitions
+
+    def most_commom_numbers_by_column(matriz, number_range: int, column=0):
+        """Retorna uma lista de tuplas: [(numero da bola: repetição desse número nos jogos anteriores)]
+        Parâmetros: Os dados , a coluna (column) e a quantidade de tuplas"""
+
+        bola_dict = {}
+        numbers, repetitions = np.unique(matriz[:, column], return_counts=True)
+        values = dict(zip(numbers, repetitions))
+        filtered_values = Counter(values).most_common(number_range)
+        bola_dict[f"Bola {column + 1}"] = filtered_values
+        return bola_dict
+
+    def get_most_repeted_numbers(matriz, frequency=5, export_data=False):
+        """Retorna um dicionário com os valores das bola e o número de repetições referentes a cada bola
+        Parâmetros: Os dados, as 5 bolas que possuem mais repetições, e a opção de exportar os dados em um arquivo json"""
+
+        num_frequent = frequency
+        frequency_dict = {}
+
+        for column in range(matriz.shape[1]):
+            unique, counts = np.unique(matriz[:, column], return_counts=True)
+            sorted_indices = np.argsort(counts)[::-1]
+
+            frequent_numbers = unique[sorted_indices[:num_frequent]]
+            frequent_counts = counts[sorted_indices[:num_frequent]]
+            frequency_dict[column+1] = {}
+
+            for i, number in enumerate(frequent_numbers):
+                frequency_dict[column+1][int(number)] = frequent_counts[i]
+        
+        if export_data:
+            with open("jogos.json", "w") as file:
+                json.dump(frequency_dict, fp=file, cls=NumpyEncoder, indent=3)
+
+        return frequency_dict
+        
+    def generate_sequences(matriz, number_range=None):
+        """Cria sequências (lista) com base nos dados gerados com os números que mais aparecem"""
+
+        frequencies = get_most_repeted_numbers(matriz, number_range)
+        keys = [list(valores.keys()) for _, valores in frequencies.items()]
+        keys = np.array(keys)
+        indices = np.random.randint(0, keys.shape[1], size=keys.shape[0])
+        sequence = keys[np.arange(keys.shape[0]), indices]
+        sequence = sequence.astype(int)
+        sequence = sequence.tolist()
+        if any((matriz[:] == np.array(sequence)).all(1)):
+            return 
+        else:
+            return sequence
+
+    # Wrapper function
+    def main():
+        data = load_data("../megasena-results.xlsx")
+        seq = int(input("Quantas sequências deseja gerar? "))
+        for s in range(0, seq):
+            new_sequence = generate_sequences(data, 5)
+            response = f"{s} - {new_sequence}"
+            print(response)
+        input(" ")
+    
+    main()
+    
+if __name__ == "__main__":
+    import pandas as pd
+    import numpy as np
+    from collections import Counter
+    from itertools import combinations
+    from encoder import NumpyEncoder
+    import json
+    import time
+
+    app()
+
+
